e724d0045f4bb8bdbc7a0746abbb2c1357e2ad3a
Sergii Demianchuk <sdemian@softserveinc.com>
2018-10-06 23:43:52 +0300

    Prepare VueJS for Phoenix Project

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,10 @@ config :planning_poker, PlanningPokerWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [
+    node: ["node_modules/.bin/webpack", "--colors", "--watch-stdin", "--progress",
+           cd: Path.expand("../assets", __DIR__)]
+  ]
 
 # ## SSL Support
 #
